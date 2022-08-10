23e6d7d02094938794bd0fc55f0cf2b92a835d4e
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-08-10 13:59:51 +0530

    commit changes

--- javascript/Assignment2/assessment1.js
+++ javascript/Assignment2/assessment1.js
@@ -1,5 +1,4 @@
-// document.body.style.backgroundColor="#e6e670";
-document.body.style.background="linear-gradient(90deg, rgba(84,100,124,1) 0%, rgba(177,167,110,1) 0%, rgba(225,230,112,1) 100%, rgba(128,210,147,0.9433656957928802) 100%)";
+document.body.style.background="linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(182,182,221,1) 46%, rgba(148,187,233,1) 100%)";
 document.body.style.justifyContent="center";
 document.body.style.textAlign="center";
 document.body.style.fontFamily="sans-serif"
