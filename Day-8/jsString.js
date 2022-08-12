f45a61c86bea4d2e1f5b069ad3f90fee75f8fda1
anshika <nisthajain2412@gmail.com>
2022-08-12 10:02:13 +0530

    commit changes

---
+++ Day-8/jsString.js
@@ -0,0 +1,15 @@
+//string literals
+var str1= "Hello World";
+var str2= 'Hello world';
+var str3= "This is Gaming's Trsining";
+console.log(str1);
+console.log(str2);
+console.log(str3);
+//using constructor
+var str= new String("This is string using constructor");
+console.log(str);
+
+
+for (const ch of str) {
+    console.log(ch);
+}
