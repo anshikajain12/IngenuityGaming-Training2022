616b15c572d79266e624655d3da3d9ba5fd4d1a3
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-08-22 14:12:52 +0530

    commit changes

--- js/todo.js
+++ js/todo.js
@@ -11,8 +11,15 @@ btn.onclick= function createToDo(e){
 
     var newLi = document.createElement('li');
     newLi.classList.add('todo_item');
-    newLi.innerHTML=todoinput.value;
-    newDiv.appendChild(newLi);
+
+    if(todoinput.value===''){
+        alert("You must write something");
+    }
+    else{
+        
+        newLi.innerHTML=todoinput.value;
+        
+        newDiv.appendChild(newLi);
 
 
     var completebtn = document.createElement('button');
@@ -28,8 +35,40 @@ btn.onclick= function createToDo(e){
 
 
     todoList.appendChild(newDiv);
+  
+    }
+    // Local Storage
 
+    localStorage.setItem("Information", JSON.stringify([...JSON.parse(localStorage.getItem("Information") || "[]"), { tasks: todoinput.value}]));
+    
+    todoinput.value="";
+    // localStorage.clear();
+    
+};
 
+todoList.onclick=function checkbtn(e){
+    var check = e.target;
+    // console.log(check);
 
+    if(check.classList[0] == 'trashbtn'){
+        // console.log("Delete button pressed");
+        var parentNode = check.parentElement;
+        // console.log(parentNode);
 
+        // remove from the localstorage
+        window.localStorage.removeItem("Information");
+
+        //remove from the session storage
+        window.sessionStorage.removeItem("Information");
+
+        parentNode.remove();
+    }
+    else if(check.classList[0] ==  'cmpbtn'){
+        // console.log("tick button pressed");
+        var parentNode = check.parentElement;
+        parentNode.classList.toggle("checked");
+    }
 }
+
+
+
