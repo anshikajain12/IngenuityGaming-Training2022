98422a0db2291eec208fc4a585c83affb05da097
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-08-25 15:53:24 +0530

    commit changes

--- Covid-19Tracker/script.js
+++ Covid-19Tracker/script.js
@@ -2,13 +2,12 @@ const API_URL = "https://api.covid19api.com/summary";
 
 function getApiData(url) {
   return new Promise((resolve, reject) => {
-
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.send();
 
     request.onload = () => {
-    //   console.log(request);
+      //   console.log(request);
 
       if (request.status === 200) {
         resolve(request.response);
@@ -26,19 +25,24 @@ getApiData(API_URL)
   })
   .catch((err) => console.log("Error" + err));
 
+function displayData(serverData) {
+  // var i=prompt("enter a number between 0 to 199");
 
+  // console.log(serverData.Countries[1].Country);
+  const s = document.createElement("select");
+  s.name = "countries";
 
-  function displayData(serverData) {
-    var i=prompt("enter a number between 0 to 199");
-    
-    console.log(serverData.Countries[1].Country);
-    document.getElementById("y1").innerHTML=serverData.Countries[i].Country;
-    document.getElementById("y2").innerHTML=serverData.Countries[i].Date;
-    document.getElementById("y3").innerHTML=serverData.Countries[i].NewDeaths;
-    document.getElementById("y4").innerHTML=serverData.Countries[i].TotalConfirmed;
-    document.getElementById("y5").innerHTML=serverData.Countries[i].TotalDeaths;
-    document.getElementById("y6").innerHTML=serverData.Countries[i].NewConfirmed;
-    }
-
-
-    
+  for (var i=0;i<serverData.Countries.length;i++) {
+    const o = document.createElement("option");
+    o.text = serverData.Countries[i].Country;
+    s.appendChild(o);
+  }
+  var x= document.querySelector("form").appendChild(s);
+  
+  
+  document.getElementById("y2").innerHTML = serverData.Countries[i].Date;
+  document.getElementById("y3").innerHTML = serverData.Countries[i].NewDeaths;
+  document.getElementById("y4").innerHTML =serverData.Countries[i].TotalConfirmed;
+  document.getElementById("y5").innerHTML = serverData.Countries[i].TotalDeaths;
+  document.getElementById("y6").innerHTML =serverData.Countries[i].NewConfirmed;
+}
