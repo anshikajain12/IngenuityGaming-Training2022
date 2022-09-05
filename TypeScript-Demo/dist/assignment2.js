5346b0f8bb99e48fa13ce08b6c8ff9f4d9a02523
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-09-05 14:21:34 +0530

    commit changes

--- TypeScript-Demo/dist/assignment2.js
+++ TypeScript-Demo/dist/assignment2.js
@@ -2,9 +2,13 @@
 document.write(`<h2> Display name in browser using typescript Dynamically</h2>`);
 let Name = prompt("Enter your Name");
 document.write(`<h3> Name is: ${Name}`);
-document.body.style.background = "linear-gradient(to right, deeppink, blue)";
-document.body.style.color = "rgb(23 9 60)";
-document.body.style.fontFamily = "sans-serif";
-document.body.style.fontSize = "20px";
+document.body.style.background = "url('https://media.istockphoto.com/photos/natural-light-wood-texture-picture-id1330622188?b=1&k=20&m=1330622188&s=170667a&w=0&h=PHfhY1iw0ETv8fyTY1kggkITNtSmya_rvk9VSdG075U=')";
+document.body.style.backgroundRepeat = "noRepeat";
+document.body.style.backgroundSize = "cover";
+document.body.style.color = "rgb(53 24 39)";
+document.body.style.fontFamily = "system-ui";
+document.body.style.fontSize = "25px";
+document.body.style.marginTop = "124px";
 document.body.style.justifyContent = "center";
 document.body.style.textAlign = "center";
+document.body.style.textShadow = "rgb(103 45 45) 5px 5px 7px";
