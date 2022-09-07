07fbe69264a311ffec62ece3d8f4983eb0f14ac3
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-09-07 10:27:05 +0530

    commit changes

--- TypeScript-Demo/dist/interface1.js
+++ TypeScript-Demo/dist/interface1.js
@@ -10,3 +10,13 @@ class Student {
 }
 let stu1 = new Student("Anshika Jain", 12345678);
 stu1.dispDetails();
+class Teacher {
+    constructor(fname, subj) {
+        this.fname = fname;
+        this.subj = subj;
+        this.fullname = fname;
+        this.subjects = subj;
+    }
+}
+let teach1 = new Teacher("Pankaj", ["Flutter", "Reactjs", "Nodejs"]);
+console.log(`Teacher Name: ${teach1.fullname} | Subject teach: ${teach1.subjects}`);
--- TypeScript-Demo/src/interface1.ts
+++ TypeScript-Demo/src/interface1.ts
@@ -11,5 +11,22 @@ class Student {
     }
 }
 
+//inferer 
 let stu1:StudentInt = new Student("Anshika Jain",12345678);
 stu1.dispDetails();
+
+interface TeacherInt{
+    fullname:string,
+    subjects:string[],
+
+}
+class Teacher implements TeacherInt{
+    fullname: string;
+    subjects: string[];
+    constructor(public fname:string, public subj:string[]){
+        this.fullname= fname;
+        this.subjects=subj;
+    }
+}
+let teach1 = new  Teacher("Pankaj",["Flutter","Reactjs","Nodejs"]);
+console.log(`Teacher Name: ${teach1.fullname} | Subject teach: ${teach1.subjects}`)
